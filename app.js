3bdf3c1edef7d63071ef66115102e713118a4fa9
DihanAbir <nahid.muradabir@gmail.com>
2021-10-04 09:35:34 -0700

    user basic model router controller and auth set up done

--- app.js
+++ app.js
@@ -1,14 +1,48 @@
 const express = require("express");
-	
-	const app = express();
-	
-	app.get("/", (req, res) => {
-	  console.log(`api runnig succesfully`);
-	});
-	
-	const PORT = process.env.PORT || 5000;
-	
-	app.listen(PORT, () => {
-	  console.log(`server running - on ${PORT}`);
-	});
- 
+const dotenv = require("dotenv");
+
+const colors = require("colors");
+const cors = require("cors");
+const connectDB = require("./config/db");
+// custome
+const errorHandler = require("./middleware/error");
+
+// Load env vars
+dotenv.config({ path: "./config/config.env" });
+
+// Connect to database
+connectDB();
+
+// Route files
+
+const users = require("./routes/users");
+
+const app = express();
+
+// Body parser
+app.use(express.json());
+
+// Enable CORS
+app.use(cors());
+
+// Mount routers
+
+app.use("/api/v1/auth", auth);
+
+app.use(errorHandler);
+
+const PORT = process.env.PORT || 5000;
+
+app.listen(
+  PORT,
+  console.log(
+    `Server running in ${process.env.NODE_ENV} mode on port ${PORT}`.yellow.bold
+  )
+);
+
+// Handle unhandled promise rejections
+process.on("unhandledRejection", (err, promise) => {
+  console.log(`Error: ${err.message}`.red);
+  // Close server & exit process
+  // server.close(() => process.exit(1));
+});
